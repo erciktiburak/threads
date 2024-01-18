3686bbbe446c855bcfbca36fbf3a9744fb024fdd
Burak Erçıktı <burakercikti@outlook.com>
2024-01-18 14:20:02 +0300

    All feature implemented

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -6,9 +6,7 @@ export const connectToDB = async () => {
   // Set strict query mode for Mongoose to prevent unknown field queries.
   mongoose.set("strictQuery", true);
 
-  if (!process.env.MONGODB_URL) {
-    throw new Error("Missing MongoDB URL. Please set the MONGODB_URL environment variable.");
-  }
+  if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
 
   // If the connection is already established, return without creating a new connection.
   if (isConnected) {
@@ -22,7 +20,6 @@ export const connectToDB = async () => {
     isConnected = true; // Set the connection status to true
     console.log("MongoDB connected");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
-    throw error; // Throw the error to be handled by the calling code
+    console.log(error);
   }
 };
